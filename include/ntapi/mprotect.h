8e4ef55885678e543bc6706da3d269baa3f6312b
pilao <zeffy@users.noreply.github.com>
2020-06-23 15:47:48 -0700

    mpr

--- include/ntapi/mprotect.h
+++ include/ntapi/mprotect.h
@@ -6,28 +6,25 @@ namespace ntapi
 {
   class mprotect
   {
-    PVOID _addr;
-    SIZE_T _len;
-    ULONG _prot;
-    NTSTATUS _st;
+    void *ptr;
+    size_t size;
+    unsigned long protect;
+    long status;
 
   public:
-    mprotect(PVOID addr, SIZE_T len, ULONG prot)
+    mprotect(void *ptr, size_t size, unsigned long protect)
+      : ptr(ptr), size(size), protect(protect), status(NtProtectVirtualMemory(NtCurrentProcess(), &ptr, &size, protect, &protect))
     {
-      _addr = addr;
-      _len = len;
-      _prot = prot;
-      _st = NtProtectVirtualMemory(NtCurrentProcess(), &_addr, &_len, _prot, &_prot);
     }
 
     ~mprotect()
     {
-      NtProtectVirtualMemory(NtCurrentProcess(), &_addr, &_len, _prot, &_prot);
+      NtProtectVirtualMemory(NtCurrentProcess(), &ptr, &size, protect, &protect);
     }
 
     operator bool() const
     {
-      return NT_SUCCESS(_st);
+      return NT_SUCCESS(status);
     }
   };
 }
