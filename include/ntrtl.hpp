209aeaada94d86749a208f035947ad277ae389c6
pilao <zeffy@users.noreply.github.com>
2020-08-12 02:04:32 -0700

    upd

--- include/ntrtl.hpp
+++ include/ntrtl.hpp
@@ -71,18 +71,7 @@ namespace nt::rtl
 
     unicode_string_view(const_pointer SourceString)
     {
-      size_type Length = 0;
-      this->Length = 0;
-      this->Buffer = const_cast<pointer>(SourceString);
-      if ( SourceString ) {
-        while ( *SourceString++ )
-          Length += sizeof(value_type);
-
-        this->Length = Length;
-        this->MaximumLength = Length + sizeof(UNICODE_NULL);
-      } else {
-        this->MaximumLength = 0;
-      }
+      THROW_IF_NTSTATUS_FAILED(RtlInitUnicodeStringEx(this, SourceString));
     }
 
     unicode_string_view(const_pointer SourceString, size_type Length)
@@ -100,7 +89,19 @@ namespace nt::rtl
     template<class T, typename = std::enable_if_t<std::is_convertible_v<T, struct _UNICODE_STRING>>>
     bool starts_with(const T &String) const
     {
-      return RtlPrefixUnicodeString(const_cast<T *>(std::addressof(String)), const_cast<unicode_string_view *>(this), FALSE);
+      auto s1 = this->begin();
+      auto s2 = String.Buffer;
+      const auto n = this->size_bytes();
+
+      if ( String.Length < n )
+        return false;
+
+      const auto end = this->end();
+      while ( s1 < end ) {
+        if ( *s1++ != *s2++ )
+          return false;
+      }
+      return true;
     }
 
     bool starts_with(PCWSTR String) const
@@ -150,7 +151,29 @@ namespace nt::rtl
     template<class T, typename = std::enable_if_t<std::is_convertible_v<T, struct _UNICODE_STRING>>>
     bool equals(const T &String) const
     {
-      return RtlEqualUnicodeString(const_cast<unicode_string_view *>(this), const_cast<T *>(std::addressof(String)), FALSE);
+      auto s1 = this->begin();
+      auto s2 = String.Buffer;
+
+      const auto n1 = this->size_bytes();
+      const auto n2 = String.Length;
+
+      if ( n1 != n2 )
+        return false;
+
+      while ( n1 >= sizeof(std::uintptr_t) ) {
+        if ( *reinterpret_cast<const std::uintptr_t *>(&*s1) != *reinterpret_cast<const std::uintptr_t *>(&*s2) )
+          break;
+
+        s1 += sizeof(std::uintptr_t) / sizeof(*s1);
+        s2 += sizeof(std::uintptr_t) / sizeof(*s2);
+      }
+
+      const auto end = this->end();
+      while ( s1 < end ) {
+        if ( *s1++ != *s2++ )
+          return false;
+      }
+      return true;
     }
 
     bool equals(PCWSTR String) const
@@ -172,10 +195,23 @@ namespace nt::rtl
     template<class T, typename = std::enable_if_t<std::is_convertible_v<T, struct _UNICODE_STRING>>>
     long compare(const T &String) const
     {
-      return RtlCompareUnicodeString(const_cast<unicode_string_view *>(this), const_cast<T *>(std::addressof(String)), FALSE);
+      auto s1 = this->begin();
+      auto s2 = String.Buffer;
+
+      const auto n1 = this->size_bytes();
+      const auto n2 = String.Length;
+
+      const auto end = this->end();
+      while ( s1 < end ) {
+        if ( *s1 != *s2 )
+          return static_cast<long>(*s1) - static_cast<long>(*s2);
+        ++s1;
+        ++s2;
+      }
+      return n1 - n2;
     }
 
-    bool compare(PCWSTR String) const
+    long compare(PCWSTR String) const
     {
       return this->compare(unicode_string_view(String));
     }
@@ -186,7 +222,7 @@ namespace nt::rtl
       return RtlCompareUnicodeString(const_cast<unicode_string_view *>(this), const_cast<T *>(std::addressof(String)), TRUE);
     }
 
-    bool icompare(PCWSTR String) const
+    long icompare(PCWSTR String) const
     {
       return this->icompare(unicode_string_view(String));
     }
@@ -280,12 +316,8 @@ namespace nt::rtl
 
     unicode_string(PCWSTR SourceString)
     {
-      RtlCreateUnicodeString(this, SourceString);
-    }
-
-    unicode_string(PCSTR SourceString)
-    {
-      RtlCreateUnicodeStringFromAsciiz(this, SourceString);
+      if ( !RtlCreateUnicodeString(this, SourceString) )
+        throw std::bad_alloc();
     }
 
     ~unicode_string()
@@ -323,7 +355,19 @@ namespace nt::rtl
     template<class T, typename = std::enable_if_t<std::is_convertible_v<T, struct _UNICODE_STRING>>>
     bool starts_with(const T &String) const
     {
-      return RtlPrefixUnicodeString(const_cast<T *>(std::addressof(String)), const_cast<unicode_string *>(this), FALSE);
+      auto s1 = this->begin();
+      auto s2 = String.Buffer;
+      const auto n = this->size_bytes();
+
+      if ( String.Length < n )
+        return false;
+
+      const auto end = this->end();
+      while ( s1 < end ) {
+        if ( *s1++ != *s2++ )
+          return false;
+      }
+      return true;
     }
 
     bool starts_with(PCWSTR String) const
@@ -372,13 +416,39 @@ namespace nt::rtl
 
     void clear()
     {
-      RtlEraseUnicodeString(this);
+      if ( this->data() == nullptr || this->capacity() == 0 )
+        return;
+
+      std::memset(this->data(), 0, this->capacity());
+      this->Length = 0;
     }
 
     template<class T, typename = std::enable_if_t<std::is_convertible_v<T, struct _UNICODE_STRING>>>
     bool equals(const T &String) const
     {
-      return RtlEqualUnicodeString(const_cast<unicode_string *>(this), const_cast<T *>(std::addressof(String)), FALSE);
+      auto s1 = this->begin();
+      auto s2 = String.Buffer;
+
+      const auto n1 = this->size_bytes();
+      const auto n2 = String.Length;
+
+      if ( n1 != n2 )
+        return false;
+
+      while ( n1 >= sizeof(std::uintptr_t) ) {
+        if ( *reinterpret_cast<const std::uintptr_t *>(&*s1) != *reinterpret_cast<const std::uintptr_t *>(&*s2) )
+          break;
+
+        s1 += sizeof(std::uintptr_t) / sizeof(*s1);
+        s2 += sizeof(std::uintptr_t) / sizeof(*s2);
+      }
+
+      const auto end = this->end();
+      while ( s1 < end ) {
+        if ( *s1++ != *s2++ )
+          return false;
+      }
+      return true;
     }
 
     bool equals(PCWSTR String) const
@@ -400,10 +470,24 @@ namespace nt::rtl
     template<class T, typename = std::enable_if_t<std::is_convertible_v<T, struct _UNICODE_STRING>>>
     long compare(const T &String) const
     {
-      return RtlCompareUnicodeString(const_cast<unicode_string *>(this), const_cast<T *>(std::addressof(String)), FALSE);
+
+      auto s1 = this->begin();
+      auto s2 = String.Buffer;
+
+      const auto n1 = this->size_bytes();
+      const auto n2 = String.Length;
+
+      const auto end = this->end();
+      while ( s1 < end ) {
+        if ( *s1 != *s2 )
+          return static_cast<long>(*s1) - static_cast<long>(*s2);
+        ++s1;
+        ++s2;
+      }
+      return n1 - n2;
     }
 
-    bool compare(PCWSTR String) const
+    long compare(PCWSTR String) const
     {
       return this->compare(unicode_string_view(String));
     }
@@ -414,7 +498,7 @@ namespace nt::rtl
       return RtlCompareUnicodeString(const_cast<unicode_string *>(this), const_cast<T *>(std::addressof(String)), TRUE);
     }
 
-    bool icompare(PCWSTR String) const
+    long icompare(PCWSTR String) const
     {
       return this->icompare(unicode_string_view(String));
     }
@@ -509,4 +593,36 @@ namespace nt::rtl
       return std::make_reverse_iterator(this->begin());
     }
   };
+
+  inline unicode_string to_unicode_string(const struct _STRING &SourceString)
+  {
+    unicode_string DestinationString;
+
+    THROW_IF_NTSTATUS_FAILED(RtlAnsiStringToUnicodeString(&DestinationString, const_cast<struct _STRING *>(std::addressof(SourceString)), TRUE));
+    return DestinationString;
+  }
+
+  inline unicode_string to_unicode_string(PCSZ SourceString)
+  {
+    struct _STRING AnsiString;
+
+    THROW_IF_NTSTATUS_FAILED(RtlInitAnsiStringEx(&AnsiString, SourceString));
+    return to_unicode_string(AnsiString);
+  }
+
+  inline unicode_string to_unicode_string(ULONGLONG Value, ULONG Base)
+  {
+    unicode_string DestinationString;
+
+    THROW_IF_NTSTATUS_FAILED(RtlInt64ToUnicodeString(Value, Base, &DestinationString));
+    return DestinationString;
+  }
+
+  inline unicode_string to_unicode_string(ULONG Value, ULONG Base)
+  {
+    unicode_string DestinationString;
+
+    THROW_IF_NTSTATUS_FAILED(RtlIntegerToUnicodeString(Value, Base, &DestinationString));
+    return DestinationString;
+  }
 }
