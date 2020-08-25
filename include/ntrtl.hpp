7d609e44c3c2b4e37dcb3c9a97ace2bc30f2b65e
pilao <zeffy@users.noreply.github.com>
2020-08-25 02:08:50 -0700

    add more rtl functions

--- include/ntrtl.hpp
+++ include/ntrtl.hpp
@@ -2,8 +2,10 @@
 #include <phnt_windows.h>
 #include <phnt.h>
 
+#include <span>
 #include <iterator>
 #include <type_traits>
+#include <memory>
 
 #include <wil/result.h>
 #include <wil/resource.h>
@@ -624,5 +626,92 @@ namespace nt::rtl
 
     THROW_IF_NTSTATUS_FAILED(RtlIntegerToUnicodeString(Value, Base, &DestinationString));
     return DestinationString;
+  }
+
+  inline PIMAGE_NT_HEADERS image_nt_headers(PVOID Base)
+  {
+    if ( Base != nullptr && Base != reinterpret_cast<PVOID>(-1) ) {
+      const auto DosHeader = reinterpret_cast<PIMAGE_DOS_HEADER>(Base);
+      if ( DosHeader->e_magic == IMAGE_DOS_SIGNATURE ) {
+        const auto NtHeaders = reinterpret_cast<PIMAGE_NT_HEADERS>(
+          reinterpret_cast<ULONG_PTR>(Base) + DosHeader->e_lfanew);
+        if ( NtHeaders->Signature == IMAGE_NT_SIGNATURE )
+          return NtHeaders;
+      }
+    }
+    return nullptr;
+  }
+
+  inline PIMAGE_SECTION_HEADER image_rva_to_section(PIMAGE_NT_HEADERS NtHeaders, PVOID Base, ULONG Rva)
+  {
+    if ( NtHeaders ) {
+      const auto NtSections = std::span{IMAGE_FIRST_SECTION(NtHeaders), NtHeaders->FileHeader.NumberOfSections};
+      const auto Iter = std::find_if(NtSections.begin(), NtSections.end(), [Rva](const IMAGE_SECTION_HEADER &NtSection) {
+        return Rva >= NtSection.VirtualAddress && Rva < NtSection.VirtualAddress + NtSection.SizeOfRawData;
+      });
+      if ( Iter != NtSections.end() )
+        return std::addressof(*Iter);
+    }
+  }
+
+  inline PIMAGE_SECTION_HEADER image_rva_to_section(PVOID Base, ULONG Rva)
+  {
+    return image_rva_to_section(image_nt_headers(Base), Base, Rva);
+  }
+
+  template<class T = VOID, typename = std::enable_if_t<std::is_void_v<T> || std::is_pod_v<T> || std::is_function_v<T>>>
+  inline std::add_pointer_t<T> image_rva_to_va(PIMAGE_NT_HEADERS NtHeaders, PVOID Base, ULONG Rva)
+  {
+    const auto NtSection = image_rva_to_section(NtHeaders, Base, Rva);
+    if ( NtSection ) {
+      return reinterpret_cast<std::add_pointer_t<T>>(
+        (reinterpret_cast<UINT_PTR>(Base) + (Rva - NtSection->VirtualAddress) + NtSection->PointerToRawData));
+    }
+    return nullptr;
+  }
+
+  template<class T = VOID, typename = std::enable_if_t<std::is_void_v<T> || std::is_pod_v<T> || std::is_function_v<T>>>
+  inline std::add_pointer_t<T> image_rva_to_va(PVOID Base, ULONG Rva)
+  {
+    return image_rva_to_va<T>(image_nt_headers(Base), Base, Rva);
+  }
+
+  inline std::span<IMAGE_RUNTIME_FUNCTION_ENTRY> lookup_function_table(PVOID ControlPc, PVOID *ImageBase)
+  {
+    const auto Lock = std::lock_guard{*static_cast<critical_section *>(NtCurrentPeb()->LoaderLock)};
+    const auto ModuleList = &NtCurrentPeb()->Ldr->InMemoryOrderModuleList;
+    for ( auto Next = ModuleList->Flink; Next != ModuleList; Next = Next->Flink ) {
+      const auto Entry = CONTAINING_RECORD(Next, LDR_DATA_TABLE_ENTRY, InMemoryOrderLinks);
+      if ( (reinterpret_cast<ULONG_PTR>(ControlPc) >= reinterpret_cast<ULONG_PTR>(Entry->DllBase))
+          && (reinterpret_cast<ULONG_PTR>(ControlPc) < reinterpret_cast<ULONG_PTR>(Entry->DllBase) + Entry->SizeOfImage) ) {
+
+        const auto NtHeaders = image_nt_headers(Entry->DllBase);
+        if ( NtHeaders->OptionalHeader.NumberOfRvaAndSizes > IMAGE_DIRECTORY_ENTRY_EXCEPTION ) {
+          const auto Ptr = image_rva_to_va<IMAGE_RUNTIME_FUNCTION_ENTRY>(NtHeaders,
+                                                                         Entry->DllBase,
+                                                                         NtHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXCEPTION].VirtualAddress);
+          const auto Size = NtHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_EXCEPTION].Size / sizeof(IMAGE_RUNTIME_FUNCTION_ENTRY);
+          if ( ImageBase )
+            *ImageBase = Entry->DllBase;
+          return std::span{Ptr, Size};
+        }
+      }
+    }
+    return {};
+  }
+
+  inline PIMAGE_RUNTIME_FUNCTION_ENTRY lookup_function_entry(PVOID ControlPc, PVOID *ImageBase)
+  {
+    const auto FunctionTable = lookup_function_table(ControlPc, ImageBase);
+    if ( !FunctionTable.empty() ) {
+      const auto RelativePc = reinterpret_cast<ULONG_PTR>(ControlPc) - reinterpret_cast<ULONG_PTR>(*ImageBase);
+      const auto Iter = std::lower_bound(FunctionTable.begin(), FunctionTable.end(), ControlPc, [](const IMAGE_RUNTIME_FUNCTION_ENTRY &FunctionEntry,
+                                                                                                   const ULONG &RelativePc) {
+        return FunctionEntry.BeginAddress >= RelativePc && RelativePc < FunctionEntry.EndAddress;
+      });
+      if ( Iter != FunctionTable.end() )
+        return std::addressof(*Iter);
+    }
+    return nullptr;
   }
 }
